f6b338fd008a73499c6f55880790ff0352cd75c8
maneulyori <maneulyori@gmail.com>
2012-05-15 18:03:23 +0900

    Adding heartbeat measuring function... Inital stage. (far to go)

---
+++ heartbeat.h
@@ -0,0 +1,19 @@
+/*
+ * heartbeat.h
+ *
+ *  Created on: 2012. 5. 15.
+ *      Author: iluvatar
+ */
+
+#ifndef HEARTBEAT_H_
+#define HEARTBEAT_H_
+
+struct Imageavg {
+	unsigned int redAverage;
+	unsigned int greenAverage;
+	unsigned int blueAverage;
+};
+
+typedef Imageavg ImageAvg;
+
+#endif /* HEARTBEAT_H_ */
