5636e037602b47e1efe12d93c56c1d8aacbde660
maneulyori <maneulyori@gmail.com>
2012-05-15 18:03:00 +0900

    Adding heartbeat measuring function... Inital stage. (far to go)

--- Debug/subdir.mk
+++ Debug/subdir.mk
@@ -4,14 +4,17 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
+../heartbeat.cpp \
 ../libfreenect_cv.cpp \
 ../main.cpp 
 
 OBJS += \
+./heartbeat.o \
 ./libfreenect_cv.o \
 ./main.o 
 
 CPP_DEPS += \
+./heartbeat.d \
 ./libfreenect_cv.d \
 ./main.d 
 
